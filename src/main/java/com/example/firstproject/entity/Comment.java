bae85bf49f80183852440bdfcb8063c614269083
junhochoi-dev <cjh970422@gmail.com>
2023-08-29 11:01:05 +0900

    springpractice 14.1, 14.2

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // Comment 엔티티와 Article 엔티티를 다대일 관계로 설정
+    @JoinColumn(name="article_id") // 외래키 생성, Article 엔티티의 기본키(id)와 매핑
+    private Article article; // 해당 댓글의 부모 게시글
+
+    @Column
+    private String nickname; // 댓글을 단 사람
+
+    @Column
+    private String body; // 댓글 본문
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,19 @@
 INSERT INTO article(title, content) VALUES('가가가가', '1111');
 INSERT INTO article(title, content) VALUES('나나나나', '2222');
 INSERT INTO article(title, content) VALUES('다다다다', '3333');
+
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는 ?', '댓글 고');
+INSERT INTO article(title, content) VALUES('당신의 소울 푸드는 ?', '댓글 고고');
+INSERT INTO article(title, content) VALUES('당신의 취미는 ?', '댓글 고고고');
+
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Park', '굿 윌 헐팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Kim', '아이엠 샘');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Choi', '쇼생크 탈출');
+
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Park', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Kim', '샤브샤브');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Choi', '초밥');
+
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Park', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Kim', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Choi', '독서');
