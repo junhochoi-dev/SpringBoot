1d8089199552135aedfd293c9bcbeb3f3e50d5b5
최준호 <cjh970422@gmail.com>
2023-09-12 17:33:00 +0900

    [FEAT] 15.1, 15.2

---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+    
+    // 1. 댓글 조회
+    @
+    // 2. 댓글 생성
+    
+    // 3. 댓글 수정
+    
+    // 4. 댓글 삭제
+
+}
